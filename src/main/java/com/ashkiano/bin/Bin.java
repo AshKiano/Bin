a897cc6a1996719c7cf23fe5294d859fb09ce758
jvyskocil <ash.kiano@gmail.com>
2024-07-22 21:49:53 +0200

    version 1.11

--- src/main/java/com/ashkiano/bin/Bin.java
+++ src/main/java/com/ashkiano/bin/Bin.java
@@ -1,5 +1,6 @@
 package com.ashkiano.bin;
 
+import com.ashkiano.ashlib.PluginStatistics;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,6 +11,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.json.JSONObject;
 
@@ -24,6 +26,14 @@ public class Bin extends JavaPlugin implements CommandExecutor, Listener {
 
     @Override
     public void onEnable() {
+        //TODO tuto chybu vypisovat i OP hráčům do chatu
+        if (!isAshLibPresent()) {
+            getLogger().severe("AshLib plugin is missing! Please download and install AshLib to run Bin. (can be downloaded from: https://www.spigotmc.org/resources/ashlib.118282/ )");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+        new PluginStatistics(this);
+
         this.saveDefaultConfig();
         binTitle = getConfig().getString("BinTitle", "Bin");
 
@@ -65,7 +75,7 @@ public class Bin extends JavaPlugin implements CommandExecutor, Listener {
     private void checkForUpdates() {
         try {
             String pluginName = this.getDescription().getName();
-            URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
+            URL url = new URL("https://plugins.ashkiano.com/version_check.php?plugin=" + pluginName);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
 
@@ -100,5 +110,10 @@ public class Bin extends JavaPlugin implements CommandExecutor, Listener {
         } catch (Exception e) {
             this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
         }
+    }
+
+    private boolean isAshLibPresent() {
+        Plugin plugin = Bukkit.getPluginManager().getPlugin("AshLib");
+        return plugin != null && plugin.isEnabled();
     }
 }
